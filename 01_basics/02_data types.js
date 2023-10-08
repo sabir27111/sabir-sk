0466244dce9eb470eb65ee0a7600ef3e231e00d3
sabir27111 <142635364+sabir27111@users.noreply.github.com>
2023-10-08 10:18:54 +0000

    data types

---
+++ 01_basics/02_data types.js
@@ -0,0 +1,34 @@
+"Use strict"; //treat all js code as newer verson /js //
+
+// alert(3+3) //we are useing nodjs, not browser
+
+console.log(3
+    +3); //code readability should be high 
+
+
+console.log("Sabir sk");
+
+let name = "sabir sk"
+let age = 20
+let isLoggedIn = false
+
+
+
+/* 
+NOTE:
+Different type of data type
+// number => 2to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefine => 
+// symble => unique
+
+
+// object 
+*/
+console.log(typeof name); // NOTE: string (output type of deta type)
+console.log(typeof age); // NOTE: number (output type of deta type)
+console.log(typeof undefined); // NOTE: undefined (output type of deta type)
+console.log(typeof null); // NOTE: object  (output type of deta type)
