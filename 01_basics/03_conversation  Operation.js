ab27719171f64ae25fc03cc27914f7ee3ea9d5f1
sabir27111 <142635364+sabir27111@users.noreply.github.com>
2023-10-09 06:08:15 +0000

    data type of conversation

---
+++ 01_basics/03_conversation  Operation.js
@@ -0,0 +1,45 @@
+let score = "sabir"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+ let valueInNumber = Number (score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
+ 
+/*   NOTE:
+  when i use this below
+ // "33" => 33
+ // "33abc" => NaN
+ // true => 1: false => 0
+// undefine => undefine
+// "sabir" => NaN
+ */
+
+
+
+let IsLoggedIn = "sabir"
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+ NOTE:
+  when i use this below 
+// 1 => true; 0 => false
+// "" => false
+// "sabir" => true
+*/
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber );
+
+/*
+ NOTE:
+  when i use this below 
+ // 33 => 33
+ typeof m=> string
+
+  */
